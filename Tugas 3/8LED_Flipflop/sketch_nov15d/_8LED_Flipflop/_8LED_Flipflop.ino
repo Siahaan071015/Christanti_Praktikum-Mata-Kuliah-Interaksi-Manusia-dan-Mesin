910616a6a8b8a619d00fc2b998412f8b117ac733
Siahaan071015 <153176718+Siahaan071015@users.noreply.github.com>
2024-05-30 11:58:01 +0700

    Add files via upload

---
+++ Tugas 3/8LED_Flipflop/sketch_nov15d/_8LED_Flipflop/_8LED_Flipflop.ino
@@ -0,0 +1,28 @@
+int ledPins[8] = { 
+  0, 1, 2, 3, 4, 5, 6, 7
+  };
+int i;
+
+void setup() {
+  // set semua pin LED sebagai OUTPUT
+  for (i = 0; i < 8; i++){
+    pinMode(ledPins[i], OUTPUT);
+  }
+
+}
+
+void loop() {
+  for (i = 0; i<8; i++){
+    digitalWrite(ledPins[i], HIGH);
+    delay(1000);
+    digitalWrite(ledPins[i], LOW);
+    delay(1000);
+  }
+  for (i = 7; i >= 0; i--){
+    digitalWrite(ledPins[i], HIGH);
+    delay(1000);
+    digitalWrite(ledPins[i], LOW);
+    delay(1000);
+  }
+
+}
